8973125d28872bf2c42c4cc9165003076fa3a75c
Alexsandro Souza <alexsandro.souza@deem.com>
2021-07-22 23:24:00 +0100

    Fix module issue

--- py_server/config/web.py
+++ py_server/config/web.py
@@ -2,7 +2,7 @@ import json
 
 
 def get_http_config():
-	with open('config/settings.json') as f:
+	with open('py_server/config/settings.json') as f:
 		settings = json.load(f)
 		server = settings["hostname"]
 		port = int(settings["port"])
