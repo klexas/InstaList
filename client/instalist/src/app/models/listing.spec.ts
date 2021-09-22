28d8cbdca0b59a44dd62acd1d97e65f4f2d69f6e
Adam Robins <iam@adamrobins.com>
2021-09-22 23:37:54 +0100

    Project cleanup for just Python

--- client/instalist/src/app/models/listing.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Listing } from './listing';
-
-describe('Listing', () => {
-  it('should create an instance', () => {
-    expect(new Listing()).toBeTruthy();
-  });
-});
--- client/instalist/src/app/models/listing.ts
+++
@@ -1,9 +0,0 @@
-export class Listing {
-  public id!: number;
-  public name!: string;
-  public description!: string;
-  public URL!: string;
-  public tags!: string[];
-  public images!: string[];
-
-}
